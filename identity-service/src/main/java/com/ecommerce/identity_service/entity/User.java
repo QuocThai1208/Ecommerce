50f1c7e0117401156c00c887255ae8e3cd10265f
Thái <thaiqp124@gmail.com>
2026-02-23 20:35:01 +0700

    feat(identity-service): add database

---
+++ identity-service/src/main/java/com/ecommerce/identity_service/entity/User.java
@@ -0,0 +1,29 @@
+package com.ecommerce.identity_service.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String id;
+
+    @Column(name = "username", unique = true, columnDefinition = "VARCHAR(255) COLLATE utf8mb4_unicode_ci")
+    String username;
+    String password;
+
+    @Column(name = "email", unique = true, columnDefinition = "VARCHAR(255) COLLATE utf8mb4_unicode_ci")
+    String email;
+
+    @ManyToMany
+    Set<Role> roles;
+}
